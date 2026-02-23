d13c6ac33f07206c19c3654110884971e4ad6f53
Gustavo Felipe de Andrade <gustavo.fa2005@aluno.ifsc.edu.br>
2026-02-23 08:47:43 -0300

    Switch Case

--- aula-04/Segundo.java
+++ aula-04/Segundo.java
@@ -1,3 +1,19 @@
+
 void main(){
-    IO.println("Olá Mundo!");
+
+    int i = 10;
+
+    switch (i) {
+        case 1:
+            IO.println("um");
+            break;
+        case 2:
+            IO.print("dois");
+            break;
+        case 3:
+            IO.print("tres");
+            break;
+        default:
+            IO.println("outro valor");
+    }
 }
