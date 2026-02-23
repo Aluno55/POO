d0022fc5cc44068862cafb3e1e31d3966d46d975
Gustavo Felipe de Andrade <gustavo.fa2005@aluno.ifsc.edu.br>
2026-02-23 08:28:06 -0300

    HelloWorld

--- README.md
+++ README.md
@@ -7,4 +7,9 @@
 ## Aulas
 - [Aula 01](./aula-01/) - Introdução à linguagem Java
 - [Aula 04](./aula-04/) - Iniciando Java
-<!-- Lembrete: mkdir, cd, git init, git clone, code. -->
+<!-- Lembrete: mkdir aula-N
+cd aula-N
+git init
+git clone https://github.com/Aluno55/POO.git
+code .
+-->
---
+++ aula-04/OlaMundo.java
@@ -0,0 +1,13 @@
+// classe é OlaMundo
+// Extension Pack for Java, baixa no VSC
+
+// int main(char* argv[], int argc){
+// printf("Olá mundo!\n");
+// return 0;
+// }
+public class OlaMundo{
+  //public permanece
+    public static void main(String[] args) {
+        System.out.println("Olá mundo");
+    }
+}
