d2f5b70ebd0e2dda328262ade418d4f9c59ea8bb
Gustavo Felipe de Andrade <gustavo.fa2005@aluno.ifsc.edu.br>
2026-02-23 09:08:26 -0300

    vetores e matriz

--- aula-04/Segundo.java
+++ aula-04/Segundo.java
@@ -1,28 +1,35 @@
 
 void main(){
 
-    int i = 10;
-    String resultado;
+    int[] vetor = new int[3];
+    vetor[0] = 1;
+    vetor [1] = 2;
+    vetor [2] = 3;
+    
+    int[][] matriz = new int[2][3];
+    matriz[0][0] = 1;
+    matriz[1][2]= 10;
 
-    switch (i) {
-        case 1:
-            resultado = "um";
-            break;
-        case 2:
-            resultado = "dois";
-            break;
-        case 3:
-            resultado = "tres";
-            break;
-        default:
-            resultado = "outro valor";
-    }
-    IO.println(resultado);
-
-    String s = switch(i){
-        case 1 -> "Um";
-        case 2 -> "Dois";
-        case 3 -> "Tres";
-        default -> "Outro";
-    };
+    // int i = 10;
+    // String resultado;
+    // switch (i) {
+    //     case 1:
+    //         resultado = "um";
+    //         break;
+    //     case 2:
+    //         resultado = "dois";
+    //         break;
+    //     case 3:
+    //         resultado = "tres";
+    //         break;
+    //     default:
+    //         resultado = "outro valor";
+    // }
+    // IO.println(resultado);
+    // String s = switch(i){
+    //     case 1 -> "Um";
+    //     case 2 -> "Dois";
+    //     case 3 -> "Tres";
+    //     default -> "Outro";
+    // };
 }
