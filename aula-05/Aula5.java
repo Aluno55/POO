44b2654b91b1382bcdeb5f1316ff474ad26d71aa
Gustavo Felipe de Andrade <gustavo.fa2005@aluno.ifsc.edu.br>
2026-03-02 08:04:41 -0300

    aula 5

---
+++ aula-05/Aula5.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+void main (){
+    
+    // String num = "2";
+    // int a = 10;
+    // a= Integer.parseInt(num);
+    // double d = Double.parseDouble(num);
+
+    String name = IO.readln("Entre com o seu nome: ");
+    IO.println("Olá! " + name);
+}
