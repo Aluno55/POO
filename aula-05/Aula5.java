987676cc925489f3ecd6801a972620adb83d0f07
Gustavo Felipe de Andrade <gustavo.fa2005@aluno.ifsc.edu.br>
2026-03-02 08:18:10 -0300

    nome e calculo de idade

--- aula-05/Aula5.java
+++ aula-05/Aula5.java
@@ -7,6 +7,9 @@ void main (){
     // a= Integer.parseInt(num);
     // double d = Double.parseDouble(num);
 
-    String name = IO.readln("Entre com o seu nome: ");
-    IO.println("Olá! " + name);
+    String name = IO.readln("Entre com o seu nome: "),
+    ano = IO.readln("Entre com o seu ano de nascimento: ");
+    IO.println("Olá! " + name); // printf tbm funciona ou println com string.format; porém ambos precisam usar %s
+    IO.println(ano);
+    IO.println(2026- Integer.parseInt(ano));
 }
