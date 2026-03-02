fdf688d202c53ca4d607711e0e2de8b5c2d6088f
Gustavo Felipe de Andrade <gustavo.fa2005@aluno.ifsc.edu.br>
2026-03-02 08:44:04 -0300

    fixed

--- aula-05/provas.java
+++ aula-05/provas.java
@@ -2,15 +2,16 @@ void main (){
     // 4 notas, de 0 a 10
     // usar for each e apresente a média
 
-    double[] notas = new double[3];
+    double[] notas = new double[4];
 
-for (int i = 1; i <= notas.length; i++) {
-    String n = IO.readln("Entre com a " + i + "a. nota: ");
-    notas[i]=Integer.parseInt(n);
+for (int i = 0; i < notas.length; i++) {
+    String n = IO.readln("Entre com a " + (i+1) + "a. nota: ");
+    notas[i]=Double.parseDouble(n);
+    // if (notas[i] > 10 || notas[i] < 0)
     }
 double soma = 0;
 for (double d : notas) {
     soma = soma + d;
     }
-IO.println(soma/4);
+IO.println(Math.round(soma/4));
 }
