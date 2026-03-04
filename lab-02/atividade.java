3c0de3ff17e60f0ebc6445a6d5fe51a6999b947a
Aluno55 <gustavo.fa2005@aluno.ifsc.edu.br>
2026-03-04 12:19:28 -0300

    estações
    provavelmente tem jeito melhor de se fazer

--- lab-02/atividade.java
+++ lab-02/atividade.java
@@ -4,24 +4,30 @@ public class atividade {
         String day = IO.readln("Entre com o dia: "), month = IO.readln("Entre com o mês: ");
     int dia = Integer.parseInt(day);
     int mes = 0;
-    if(month.equals("março")){mes = 3;}
-    else if (month.equals("junho")){mes = 6;}
-    else if (month.equals("setembro")){mes = 8;}
-    else if (month.equals("dezembro")){mes =12;}
+    if (month.equalsIgnoreCase("janeiro")) {mes = 1;}
+    else if (month.equalsIgnoreCase("fevereiro")) {mes = 2;}
+    else if (month.equalsIgnoreCase("março")) {mes = 3;}
+    else if (month.equalsIgnoreCase("abril")) {mes = 4;}
+    else if (month.equalsIgnoreCase("maio")) {mes = 5;}
+    else if (month.equalsIgnoreCase("junho")) {mes = 6;}
+    else if (month.equalsIgnoreCase("julho")) {mes = 7;}
+    else if (month.equalsIgnoreCase("agosto")) {mes = 8;}
+    else if (month.equalsIgnoreCase("setembro")) {mes = 9;}
+    else if (month.equalsIgnoreCase("outubro")) {mes = 10;}
+    else if (month.equalsIgnoreCase("novembro")) {mes = 11;}
+    else if (month.equalsIgnoreCase("dezembro")) {mes = 12;}
+    else {
+        IO.println("mes inválida");
+        dia = 0;
+    }
     
-    switch (mes) {
-        case 3: if (dia > 20){break;}
-        case 1:
-        case 2:
-            System.out.println("Verão");
-            break;
-        case 4: case 5:
-        default:
-            break;
-    }
-    }
-// Entre com o dia: 21
-// Entre com o mês: março
-// # Saída:
-// Outono
-}
+    if ((mes == 12 && dia >= 21) || (mes <= 3 && (mes < 3 || dia <= 20))) {
+        IO.println("verão");}
+    else if ((mes == 3 && dia >= 21) || (mes >= 4 && mes <= 5) || (mes == 6 && dia <= 20)) {
+        IO.println("outono");}
+    else if ((mes == 6 && dia >= 21) || (mes >= 7 && mes <= 8) || (mes == 9 && dia <= 22)) {
+        IO.println("inverno");}
+    else if ((mes == 9 && dia >= 23) || (mes >= 10 && mes <= 11) || (mes == 12 && dia <= 20)) {
+        IO.println("primavera");}
+    else {IO.println("data inválida");}   
+}}
