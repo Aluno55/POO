64c31f1160ef8774cdb0773f3c4c34fcf61cef68
Gustavo Felipe de Andrade <gustavo.fa2005@aluno.ifsc.edu.br>
2026-03-04 10:58:45 -0300

    lendo terminal argumentos

---
+++ aula-06/Argumentos.java
@@ -0,0 +1,30 @@
+public class Argumentos {
+
+
+
+
+
+
+public static void main(String[] args) {
+    if (args.length != 2){
+        System.out.println("erro");
+    }
+    else {
+        System.out.println("Bom dia " + args[0]);
+        System.out.println(args[1]);
+    }
+
+
+   
+}
+
+
+
+
+
+
+
+
+
+
+}
--- aula-06/segundo/app/src/main/java/engtelecom/poo/App.java
+++ aula-06/segundo/app/src/main/java/engtelecom/poo/App.java
@@ -1,21 +1,22 @@
 package engtelecom.poo;
 
-import java.util.Random;
-
 public class App {
 
     public static void main(String[] args) {
-        Random r = new Random();
-        int num = 0;
-        for (int i = 0; i < 5 ; i++){
-            num = r.nextInt(10);
-            System.out.println(num);
-        }
-//        int num = r.nextInt();
-//        System.out.println(num);
+        String nome = IO.readln("Entre com seu nome: ");
+        IO.println("Bom dia "+ nome);
 
-
-
-
+        //Exemplo com strings
+//        String a = "POO", b = "ifsc";
+//        int i = 10, j = 20;
+//        if (!a.equalsIgnoreCase(b)){
+//            System.out.println("não");
+//        } else {
+//            System.out.println("sim");
+//        }
+//        String d = "Eng" + "Telecom " + b + " " + a + i;
+//
+//        String jav = "java".repeat(100);
+//        System.out.println(jav);
     }
 }
