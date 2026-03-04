787b9016eb2024f66b102e4118ea7700f5ff96f5
Gustavo Felipe de Andrade <gustavo.fa2005@aluno.ifsc.edu.br>
2026-03-04 10:30:35 -0300

    random

--- aula-06/segundo/app/src/main/java/engtelecom/poo/App.java
+++ aula-06/segundo/app/src/main/java/engtelecom/poo/App.java
@@ -1,8 +1,21 @@
 package engtelecom.poo;
 
+import java.util.Random;
+
 public class App {
 
     public static void main(String[] args) {
+        Random r = new Random();
+        int num = 0;
+        for (int i = 0; i < 5 ; i++){
+            num = r.nextInt(10);
+            System.out.println(num);
+        }
+//        int num = r.nextInt();
+//        System.out.println(num);
+
+
+
 
     }
 }
