7e0f63418e04fe22a3b740360e0d30ea2517c4f4
Gustavo Felipe de Andrade <gustavo.fa2005@aluno.ifsc.edu.br>
2026-03-06 14:07:51 -0300

    ret angulo

--- lab-02/AsciiArt.java
+++ lab-02/AsciiArt.java
@@ -1,23 +1,34 @@
 public class AsciiArt {
     public static void main(String[] args) {
-    String shape = new String(args[1]);
-    int tam = Integer.parseInt(args[2]), retangulo = Integer.parseInt(args[3]);    
+    String shape = new String(args[0]);
+    int tam = Integer.parseInt(args[1]), retangulo = Integer.parseInt(args[2]);
+    if (tam > 0){  
     if (shape.equalsIgnoreCase("triangulo")){
         for (int i = 1; i <= tam; i++) {
             String repetida = "*".repeat(i); 
             System.out.println(repetida);
         }
+    } else if (shape.equalsIgnoreCase("retangulo")){
+        for (int i = 1; i <= tam; i++) {
+            if (i == 1 || i == tam){
+                String repetida = "*".repeat(retangulo); 
+                System.out.println(repetida);
+            } else {
+                for (int j = 1; j <= retangulo; j++) {
+                    if (j == 1 || j ==retangulo){System.out.print("*");}
+                    else {System.out.print(" ");}
+                }
+                System.out.println();
+            } 
+        }
 
-    }
-    else if (shape.equalsIgnoreCase("retangulo")){
+    } else if (shape.equalsIgnoreCase("losango") && (tam % 2 != 0)){
+        for (int i = 0; i < tam; i++) {
+            
+        }
 
-
-    }
-    else if (shape.equalsIgnoreCase("losango")){
-
-
-    }
-
+    } else {System.out.println("Invalido");}
+    } else {System.out.println("Invalido");}
 
         
 
