5f8b6193c869a81d2e435b3de4cb2ce87c9e116d
Gustavo Felipe de Andrade <gustavo.fa2005@aluno.ifsc.edu.br>
2026-03-06 14:44:17 -0300

    los ango + alguns detalhes

--- lab-02/AsciiArt.java
+++ lab-02/AsciiArt.java
@@ -1,14 +1,18 @@
 public class AsciiArt {
+
     public static void main(String[] args) {
-    String shape = new String(args[0]);
-    int tam = Integer.parseInt(args[1]), retangulo = Integer.parseInt(args[2]);
+    String shape = args[0];
+    int tam = Integer.parseInt(args[1]);
     if (tam > 0){  
+
     if (shape.equalsIgnoreCase("triangulo")){
         for (int i = 1; i <= tam; i++) {
             String repetida = "*".repeat(i); 
             System.out.println(repetida);
         }
+
     } else if (shape.equalsIgnoreCase("retangulo")){
+        int retangulo = Integer.parseInt(args[2]);
         for (int i = 1; i <= tam; i++) {
             if (i == 1 || i == tam){
                 String repetida = "*".repeat(retangulo); 
@@ -23,8 +27,12 @@ public class AsciiArt {
         }
 
     } else if (shape.equalsIgnoreCase("losango") && (tam % 2 != 0)){
+        int meio = tam/2;
         for (int i = 0; i < tam; i++) {
-            
+            int space = Math.abs(meio - i);
+            int pontos = tam - 2 * space;
+            String simbolos = "*".repeat(pontos), espacos = " ".repeat(space);
+            System.out.println(espacos + simbolos);
         }
 
     } else {System.out.println("Invalido");}
