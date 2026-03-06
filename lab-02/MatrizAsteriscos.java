c1c13ad90b9545289c6850edc93a24322317f39f
Gustavo Felipe de Andrade <gustavo.fa2005@aluno.ifsc.edu.br>
2026-03-06 15:06:33 -0300

    cansado, terminar mais tarde

---
+++ lab-02/MatrizAsteriscos.java
@@ -0,0 +1,10 @@
+import java.util.Random;
+
+public class MatrizAsteriscos {
+    public static void main(String[] args) {
+        // Random r = new Random();
+        
+        // matriz 9 por 9
+        // encher de pontos, selecionar 2 numeros aleatorios e colocar asterico
+    }
+}
