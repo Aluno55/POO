9b645bf7246b9f68b0d93472e12479484c200315
Gustavo Felipe de Andrade <gustavo.fa2005@aluno.ifsc.edu.br>
2026-03-08 17:30:45 -0300

    matriz?

--- lab-02/LeitorArquivo.java
+++ lab-02/LeitorArquivo.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
+
 public class LeitorArquivo {
     
     public static void main(String[] args) {
-        
-        //incerteza de como faz
+        Scanner scan = new Scanner(System.in);
+        String[][] matriz = new String[9][9];
+        int i = 0;
+        while (scan.hasNext() && i < 9) {
+            String linha = scan.nextLine();
+            for (int j = 0; j < 9; j++) {
+                matriz[i][j] = linha.charAt(j);
+            }
+            i++;
+        }
     }
 }
