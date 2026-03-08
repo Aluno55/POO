651556fa0f4855b3646a2cb19656e7451d4205e2
Gustavo Felipe de Andrade <gustavo.fa2005@aluno.ifsc.edu.br>
2026-03-08 16:20:04 -0300

    questão 4; há error nos asteriscos

--- lab-02/MatrizAsteriscos.java
+++ lab-02/MatrizAsteriscos.java
@@ -2,9 +2,23 @@ import java.util.Random;
 
 public class MatrizAsteriscos {
     public static void main(String[] args) {
-        // Random r = new Random();
-        
-        // matriz 9 por 9
-        // encher de pontos, selecionar 2 numeros aleatorios e colocar asterico
+        Random r = new Random();
+        String[][] s = new String[9][9];
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                s[i][j]=".";
+            }
+        }
+        for (int i = 0; i < 10; i++) {
+            int n1 = r.nextInt(9), n2 = r.nextInt(9);
+            s[n1][n2]="*";
+        }
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                System.out.print(s[i][j]);
+            }
+            System.out.println();
+        }
     }
 }
