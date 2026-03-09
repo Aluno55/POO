61d07b8ec854dd1fde8f7b70119a3649fcddf048
Gustavo Felipe de Andrade <gustavo.fa2005@aluno.ifsc.edu.br>
2026-03-09 09:02:32 -0300

    matriz

--- lab-02/quinto/app/src/main/java/engtelecom/poo/ConverteString.java
+++ lab-02/quinto/app/src/main/java/engtelecom/poo/ConverteString.java
@@ -5,9 +5,13 @@ import java.util.Scanner;
     public static void main(String[] args){
         Scanner entrada = new Scanner(System.in);
         int[][] matriz = new int[9][9];
+        int j=0;
         while(entrada.hasNext()){
             String linha = entrada.nextLine();
-            
+            for (int i = 0; i<9; i++) {
+                matriz [j][i] = linha.charAt(i);
+            }
+            j++;
             System.out.println(linha.toUpperCase());
         }
     }
