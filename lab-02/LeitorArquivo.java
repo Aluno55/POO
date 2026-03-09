a74eb1c06659e2c53efa4caf558b87f964aeab27
Gustavo Felipe de Andrade <gustavo.fa2005@aluno.ifsc.edu.br>
2026-03-09 08:27:05 -0300

    mini alterações

--- lab-02/LeitorArquivo.java
+++ lab-02/LeitorArquivo.java
@@ -4,15 +4,12 @@ public class LeitorArquivo {
     
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        String[][] matriz = new String[9][9];
-        int i = 0;
+        
         while (scan.hasNext() && i < 9) {
             String linha = scan.nextLine();
-            for (int j = 0; j < 9; j++) {
-                matriz[i][j] = linha.charAt(j);
-            }
-            //for feito por gpt
-            i++;
+            
+
+
         }
     }
 }
