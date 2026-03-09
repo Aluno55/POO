28560f5550469a8c8389500f445422ff7402c64c
Gustavo Felipe de Andrade <gustavo.fa2005@aluno.ifsc.edu.br>
2026-03-09 09:21:39 -0300

    campo minado

--- lab-02/quinto/app/src/main/java/engtelecom/poo/ConverteString.java
+++ lab-02/quinto/app/src/main/java/engtelecom/poo/ConverteString.java
@@ -9,10 +9,22 @@ import java.util.Scanner;
         while(entrada.hasNext()){
             String linha = entrada.nextLine();
             for (int i = 0; i<9; i++) {
-                matriz [j][i] = linha.charAt(i);
+                matriz [j][i] = linha.charAt(i) == '.' ? 0 : 9;
+                if (j > 0 ) {
+
+                }
             }
+            /*  i-1     i   i+2
+            j-1 |     |     |    
+            j   |     |     | 
+            j+1 |     |     |    
+            */
+
             j++;
             System.out.println(linha.toUpperCase());
         }
+        entrada.close();
     }
 }
+
+//gradle init --package=engtelecom.poo
