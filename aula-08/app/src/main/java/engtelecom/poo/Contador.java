1cd8fc17c1897dd3aaeaae18efb4c7f5765682ab
Gustavo Felipe de Andrade <gustavo.fa2005@aluno.ifsc.edu.br>
2026-03-16 09:11:56 -0300

    um monte de coisas

---
+++ aula-08/app/src/main/java/engtelecom/poo/Contador.java
@@ -0,0 +1,8 @@
+package engtelecom.poo;
+
+public class Contador {
+    private int valorAtual = 0;
+    public void increaseByOne(){valorAtual++;}
+    public void setValorAtual(int a){valorAtual=a;}
+    public int getValue(){return valorAtual;}
+}
