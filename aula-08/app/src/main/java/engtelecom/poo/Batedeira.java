229627176539525fe79ba03ba2884ae3fea727bd
Gustavo Felipe de Andrade <gustavo.fa2005@aluno.ifsc.edu.br>
2026-03-16 13:35:03 -0300

    batedeira

--- aula-08/app/src/main/java/engtelecom/poo/Batedeira.java
+++ aula-08/app/src/main/java/engtelecom/poo/Batedeira.java
@@ -2,5 +2,8 @@ package engtelecom.poo;
 
 public class Batedeira {
     private String gancho = "nenhum";
-    
+    private int speed = 0;
+    private boolean OnOff = 0;
+
+    public void setSpeed (int a){speed = a;}
 }
