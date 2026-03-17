e39c5b9d0fdc2e6f68c4c8fdc561ef5f702498e6
Gustavo Felipe de Andrade <gustavo.fa2005@aluno.ifsc.edu.br>
2026-03-17 15:05:07 -0300

    provas e média

--- aula-08/app/src/main/java/engtelecom/poo/Disciplina.java
+++ aula-08/app/src/main/java/engtelecom/poo/Disciplina.java
@@ -4,12 +4,20 @@ public class Disciplina {
     private int numeroProvas = 1;
     private double[] prova;
 
-    public double[] getNotaProva (){return prova;}
-    public void setNotaProva (double a){
-        prova = a;
-        numeroProvas++;
+    public double getNotaProva (int qualProva){return prova[qualProva];}
+
+    public void setNotaProva (double nota){
+        double[prova.length] novo;
+        if (numeroProvas < prova.length){
+            prova(numeroProvas) = nota;
+            numeroProvas++;
+        }
     }
-    public double Media(){
-        
+
+    public double Media (){
+    double soma = 0;
+        for (int i = 0; i < numeroProvas; i++)
+            {soma += provas[i];}
+        return ((soma + atividade) / (numeroProvas + 1));    
     }
 }
