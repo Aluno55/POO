679da2588369691a8398032cef88f45b538afb87
Gustavo Felipe de Andrade <gustavo.fa2005@aluno.ifsc.edu.br>
2026-03-18 10:46:38 -0300

    override, tostrings e constructs

--- aula-10/app/src/main/java/engtelecom/poo/App.java
+++ aula-10/app/src/main/java/engtelecom/poo/App.java
@@ -2,14 +2,8 @@ package engtelecom.poo;
 
 public class App {
     public static void main(String[] args) {
-        Carro c = new Carro();
-        c.acelerar(500);
-        System.out.println(c.getVelocidadeAtual());
-        c.frear(510);
-        System.out.println(c.getVelocidadeAtual());
-        
-        Pessoa p = new Pessoa();
-        Pessoa b = new Pessoa();
-        p.getNome();
+
+        Pessoa p = new Pessoa("Jon","0123456789","jon@gar.field");
+        System.out.println(p);
     }
 }
--- aula-10/app/src/main/java/engtelecom/poo/Carro.java
+++ aula-10/app/src/main/java/engtelecom/poo/Carro.java
@@ -3,10 +3,21 @@ package engtelecom.poo;
 public class Carro {
     private String modelo;
     private double velocidadeAtual = 0;
+    private double velocidadeMax;
 
-    public double getVelocidadeAtual() {
-        return velocidadeAtual;
+    public Carro(String modelo, double velocidadeMax) {
+        this.modelo = modelo;
+        this.velocidadeMax = velocidadeMax;
     }
+
+    public String getModelo() {return modelo;}
+    public void setModelo(String modelo) {this.modelo = modelo;}
+
+    public double getVelocidadeAtual() {return velocidadeAtual;}
+    public void setVelocidadeAtual(double velocidadeAtual) {this.velocidadeAtual = velocidadeAtual;}
+    
+    public double getVelocidadeMax() {return velocidadeMax;}
+    public void setVelocidadeMax(double velocidadeMax) {this.velocidadeMax = velocidadeMax;}
 
     //velocidade maxima de 200 e minimo de 0
     public void acelerar(double a) {
