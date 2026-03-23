4184204105ff2c579ee9d50a1e54421d6d9f1a51
Gustavo <gustavo.fa2005@aluno.ifsc.edu.br>
2026-03-23 07:46:19 -0300

    getset

---
+++ lab-03/atividadeOne/app/src/main/java/engtelecom/poo/Ponto.java
@@ -0,0 +1,34 @@
+package engtelecom.poo;
+
+public class Ponto {
+    private double xPonto1;
+    private double xPonto2;
+    private double yPonto1;
+    private double yPonto2;
+
+    public void setxPonto1(double xPonto1) {
+        this.xPonto1 = xPonto1;
+    }
+    public double getxPonto1() {
+        return xPonto1;
+    }
+    public void setxPonto2(double xPonto2) {
+        this.xPonto2 = xPonto2;
+    }
+    public double getxPonto2() {
+        return xPonto2;
+    }
+    public void setyPonto1(double yPonto1) {
+        this.yPonto1 = yPonto1;
+    }
+    public double getyPonto1() {
+        return yPonto1;
+    }
+    public void setyPonto2(double yPonto2) {
+        this.yPonto2 = yPonto2;
+    }
+    public double getyPonto2() {
+        return yPonto2;
+    }
+    
+}
