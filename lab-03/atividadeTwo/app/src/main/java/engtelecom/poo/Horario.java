e4ead482129e4fafa9b23a96ae5c77f978ca980d
Gustavo <gustavo.fa2005@aluno.ifsc.edu.br>
2026-03-23 08:20:48 -0300

    atividade horario parcial 2

--- lab-03/atividadeTwo/app/src/main/java/engtelecom/poo/Horario.java
+++ lab-03/atividadeTwo/app/src/main/java/engtelecom/poo/Horario.java
@@ -6,24 +6,44 @@ public class Horario {
     private int segundo=0;
 
     public void setHorario(int hora, int minuto, int segundo) {
-        if (segundo >=60 || segundo < 0){
-            this.segundo = 0;
-        }
-        else{this.segundo = segundo;}
-
-        if (minuto >=60 || minuto < 0){
-            this.minuto = 0;
-        }
-        else{this.minuto = minuto;}
-        
+        setHora(hora);
+        setMinuto(minuto);
+        setSegundo(segundo);
+    }
+  
+    public int getHora() {
+        return hora;
+    }
+    public void setHora(int hora) {
         if (hora >= 24 || hora < 0){
             this.hora = 0;
         }
         else {this.hora = hora;}
     }
-
+    public int getMinuto() {
+        return minuto;
+    }
+    public void setMinuto(int minuto) {
+        if (minuto >=60 || minuto < 0){
+            this.minuto = 0;
+        }
+        else{this.minuto = minuto;}
+    }
+    public int getSegundo() {
+        return segundo;
+    }
+    public void setSegundo(int segundo) {
+        if (segundo >=60 || segundo < 0){
+            this.segundo = 0;
+        }
+        else{this.segundo = segundo;}
+    }
     public long toSeconds(){
         return this.hora * 3600 + this.minuto * 60 + segundo;
+    }
+
+    public long distance(Horario b) {
+        return Math.abs(toSeconds() - b.toSeconds());
     }
 
     public void setHorario(int hora, int minuto){
