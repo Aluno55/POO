7d5816ce9c7998ce2fd54a404d7b48f9665f692f
Gustavo <gustavo.fa2005@aluno.ifsc.edu.br>
2026-03-23 08:10:45 -0300

    atividade horario parcial

---
+++ lab-03/atividadeTwo/app/src/main/java/engtelecom/poo/Horario.java
@@ -0,0 +1,37 @@
+package engtelecom.poo;
+
+public class Horario {
+    private int hora=0;
+    private int minuto=0;
+    private int segundo=0;
+
+    public void setHorario(int hora, int minuto, int segundo) {
+        if (segundo >=60 || segundo < 0){
+            this.segundo = 0;
+        }
+        else{this.segundo = segundo;}
+
+        if (minuto >=60 || minuto < 0){
+            this.minuto = 0;
+        }
+        else{this.minuto = minuto;}
+        
+        if (hora >= 24 || hora < 0){
+            this.hora = 0;
+        }
+        else {this.hora = hora;}
+    }
+
+    public long toSeconds(){
+        return this.hora * 3600 + this.minuto * 60 + segundo;
+    }
+
+    public void setHorario(int hora, int minuto){
+        setHorario(hora, minuto, segundo);}
+    public void setHorario(int hora) {
+        setHorario(hora, minuto, segundo);}
+
+    public String toString(){ 
+        return String.format("%02d:%02d:%02d", hora, minuto, segundo);
+    }
+}
