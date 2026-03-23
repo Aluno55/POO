c5fec6ba0f6fc5146b4a90af4999ea4043a3c0ef
Gustavo <gustavo.fa2005@aluno.ifsc.edu.br>
2026-03-23 09:03:34 -0300

    extenso

--- lab-03/atividadeTwo/app/src/main/java/engtelecom/poo/App.java
+++ lab-03/atividadeTwo/app/src/main/java/engtelecom/poo/App.java
@@ -2,6 +2,8 @@ package engtelecom.poo;
 
 public class App {
     public static void main(String[] args) {
-        
+        Horario h = new Horario(20, 2);
+        System.out.println(h.toExtenso());
+
     }
 }
--- lab-03/atividadeTwo/app/src/main/java/engtelecom/poo/Horario.java
+++ lab-03/atividadeTwo/app/src/main/java/engtelecom/poo/Horario.java
@@ -5,38 +5,56 @@ public class Horario {
     private int minuto=0;
     private int segundo=0;
 
-    public void setHorario(int hora, int minuto, int segundo) {
+    public Horario(int hora, int minuto, int segundo) {
         setHora(hora);
         setMinuto(minuto);
         setSegundo(segundo);
     }
+
+    public Horario(int hora, int minuto){
+        this(hora, minuto, 0);}
+
+    public Horario(int hora) {
+        this(hora, 0,0);}
   
     public int getHora() {
         return hora;
     }
-    public void setHora(int hora) {
+    public boolean setHora(int hora) {
         if (hora >= 24 || hora < 0){
             this.hora = 0;
+            return false;
         }
-        else {this.hora = hora;}
+        else {
+            this.hora = hora;
+            return true;
+        }
     }
     public int getMinuto() {
         return minuto;
     }
-    public void setMinuto(int minuto) {
+    public boolean setMinuto(int minuto) {
         if (minuto >=60 || minuto < 0){
             this.minuto = 0;
+            return false;
         }
-        else{this.minuto = minuto;}
+        else{
+            this.minuto = minuto;
+            return true;
+        }
     }
     public int getSegundo() {
         return segundo;
     }
-    public void setSegundo(int segundo) {
+    public boolean setSegundo(int segundo) {
         if (segundo >=60 || segundo < 0){
             this.segundo = 0;
+            return false;
         }
-        else{this.segundo = segundo;}
+        else{
+            this.segundo = segundo;
+            return true;
+        }
     }
     public long toSeconds(){
         return this.hora * 3600 + this.minuto * 60 + segundo;
@@ -46,10 +64,39 @@ public class Horario {
         return Math.abs(toSeconds() - b.toSeconds());
     }
 
-    public void setHorario(int hora, int minuto){
-        setHorario(hora, minuto, segundo);}
-    public void setHorario(int hora) {
-        setHorario(hora, minuto, segundo);}
+    private String conversorExtenso(int valor){
+        return switch (valor) {
+            case 1 -> "um";
+            case 2 -> "duas";
+            case 10 -> "dez";
+            case 3 -> "tres";
+            case 4 -> "quatro";
+            case 5 -> "cinco";
+            case 6 -> "seis";
+            case 7 -> "sete";
+            case 8 -> "oito";
+            case 9 -> "nove";
+            case 11 -> "onze";
+            case 12 -> "doze";
+            case 13 -> "treze";
+            case 14 -> "quatorze";
+            case 15 -> "quinze";
+            case 16 -> "dezesseis";
+            case 17 -> "dezesseis";
+            case 18 -> "dezoito";
+            case 19 -> "dezenove";
+            case 20 -> "vinte";
+            default -> "";
+        };
+    }
+
+    public String toExtenso(){
+        String horas = (this.hora == 1)? "uma" : (this.hora ==2)? "duas" : conversorExtenso((hora/10)*10) + " " + conversorExtenso(hora%10);
+        String minutos = conversorExtenso((minuto/10)*10) + " " + conversorExtenso(minuto%10);
+        String segundos = conversorExtenso((segundo/10)*10) + " " + conversorExtenso(segundo%10);
+        
+        return horas + " horas e " + minutos + " minutos e " + segundos + " segundos";
+    }
 
     public String toString(){ 
         return String.format("%02d:%02d:%02d", hora, minuto, segundo);
