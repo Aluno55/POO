6941b7183d3a6cbf561f3c5c6aa187ef053b3ced
Gustavo <gustavo.fa2005@aluno.ifsc.edu.br>
2026-03-23 07:57:52 -0300

    ponto funcionando aparentemente

--- lab-03/atividadeOne/app/src/main/java/engtelecom/poo/App.java
+++ lab-03/atividadeOne/app/src/main/java/engtelecom/poo/App.java
@@ -2,6 +2,11 @@ package engtelecom.poo;
 
 public class App {
     public static void main(String[] args) {
-    
+        Ponto a = new Ponto(0, 0);
+        Ponto b = new Ponto(0, 0);
+        a.setX(3);
+        a.setY(4);
+        System.out.println(a.distancia(a, b));
+        System.out.println(b.distancia(b, a));
     }
 }
