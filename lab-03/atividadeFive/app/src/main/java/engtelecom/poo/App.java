b20d711510633d8d5a40c42a601ee7be976da41f
Gustavo <gustavo.fa2005@aluno.ifsc.edu.br>
2026-03-25 11:07:31 -0300

    robozin

--- lab-03/atividadeFive/app/src/main/java/engtelecom/poo/App.java
+++ lab-03/atividadeFive/app/src/main/java/engtelecom/poo/App.java
@@ -2,6 +2,11 @@ package engtelecom.poo;
 
 public class App {
     public static void main(String[] args) {
-
+        Robo a = new Robo("Bip");
+        Robo b = new Robo("Bop");
+        Robo c = new Robo("Robocop"); 
+        System.out.println(a.getSerie());
+        System.out.println(a.getCounter());
+        System.out.println(b.getSerie());
     }
 }
--- lab-03/atividadeFive/app/src/main/java/engtelecom/poo/Robo.java
+++ lab-03/atividadeFive/app/src/main/java/engtelecom/poo/Robo.java
@@ -1,14 +1,18 @@
 package engtelecom.poo;
 
 public class Robo {
-    String nome = "";
-    int serie = 0;
+    private String nome = "";
+    private int serie = 0;
+    private static int contador = 0;
+
     public Robo(String nome) {
         this.nome = nome;
-        this.serie++;
+        ++contador;
+        serie = contador;
     }
     public String getNome() {return nome;}
     public void setNome(String nome) {this.nome = nome;}
-    
+
     public int getSerie() {return serie;}
+    public int getCounter() {return contador;}
 }
