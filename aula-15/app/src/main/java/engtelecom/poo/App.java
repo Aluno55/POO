85bdbb305082ddb9838869517c75f430a864d0ff
Gustavo Felipe de Andrade <gustavo.fa2005@aluno.ifsc.edu.br>
2026-04-01 10:38:21 -0300

    LAMBDA! I REMEMBER LAMBDA DARNIT!

--- aula-15/app/src/main/java/engtelecom/poo/App.java
+++ aula-15/app/src/main/java/engtelecom/poo/App.java
@@ -5,19 +5,27 @@ import java.util.ArrayList;
 public class App {
     public static void main(String[] args) {
         // List
-        var s = "Gustaff"; // var é o programa adivinhando qual tipo de variavel é
+        var s = "João"; // var é o programa adivinhando qual tipo de variavel é
         ArrayList<String> l = new ArrayList<>();
-        l.add("Phillip");
-        l.add("d'Andrade");
-        l.add("Baka");
+        l.add("Maria");
+        l.add("Jorge");
+        l.add("Pedro");
         l.add(0, s);
         System.out.println(l);
-        for (String string : l) {
-            if (string.equals("Baka")) {
-                System.out.println(string);
-            }
-        }
-        
+        // for (String string : l) {
+        //     if (string.equals("Baka")) {
+        //         System.out.println(string);
+        //     }
+        // }
+
+        l.forEach(e -> {
+            if (e.equals("Maria"))
+            {System.out.println(e);}
+        }); // LAMBDA! EU LEMBRO DE VC DESGRAÇA! (x, fx)
+        l.remove("Maria");
+        l.removeIf(e -> e.equals("Jorge"));
+        System.out.println(l);
+
         // Set
         // Queue
         // Map
