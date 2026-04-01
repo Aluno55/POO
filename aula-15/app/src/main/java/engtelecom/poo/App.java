fcdbf456b88cd8900e53a39c0152dd850e50d37a
Gustavo Felipe de Andrade <gustavo.fa2005@aluno.ifsc.edu.br>
2026-04-01 10:23:16 -0300

    for each X in Y

--- aula-15/app/src/main/java/engtelecom/poo/App.java
+++ aula-15/app/src/main/java/engtelecom/poo/App.java
@@ -4,11 +4,22 @@ import java.util.ArrayList;
 
 public class App {
     public static void main(String[] args) {
-    // List
-    ArrayList<String> l = new ArrayList<>();
-    
-    // Set
-    // Queue
-    // Map
+        // List
+        var s = "Gustaff"; // var é o programa adivinhando qual tipo de variavel é
+        ArrayList<String> l = new ArrayList<>();
+        l.add("Phillip");
+        l.add("d'Andrade");
+        l.add("Baka");
+        l.add(0, s);
+        System.out.println(l);
+        for (String string : l) {
+            if (string.equals("Baka")) {
+                System.out.println(string);
+            }
+        }
+        
+        // Set
+        // Queue
+        // Map
     }
 }
