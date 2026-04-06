63e89769957959f9c03c4da6228b52fac2c9bece
gustavo <gustavo.fa2005@aluno.ifsc.edu.br>
2026-04-06 09:19:06 -0300

    remover livro, terminar em casa

--- aula-16/app/src/main/java/engtelecom/poo/App.java
+++ aula-16/app/src/main/java/engtelecom/poo/App.java
@@ -13,10 +13,10 @@ public class App {
         3- consultar livro por isbn, imprimir todos os dados
     4- consultar livro por autor, imprimir todos os dados
     5- atualizar dados do livro, menos isbn
-    6- remover livro por isbn
+        6- remover livro por isbn
         7- sair
         */
-            option = Integer.parseInt(IO.readln("Escolha uma das opções:\n1-Criar livro\n2-Listagem de Títulos com ISBN\n3-Consultar Livro pelo ISBN\n7-Sair\n"));
+            option = Integer.parseInt(IO.readln("Escolha uma das opções:\n1-Criar livro\n2-Listagem de Títulos com ISBN\n3-Consultar Livro pelo ISBN\n6-Remover livro\n7-Sair\n"));
             if (option == 1){
                 String author = IO.readln("Entre com o Autor: ");
                 String isbn = IO.readln("Entre com o ISBN: ");
@@ -24,21 +24,18 @@ public class App {
                 int ano = Integer.parseInt(IO.readln("Entre com o ano de publicação:"));
                 Livro a = new Livro(titulo, author, isbn, ano);
                 acervo.put(a.getIsbn(), a);
-            }
-
+            } 
             if (option == 2){
                 acervo.forEach((k, v)->{
                     System.out.println("ISBN: " + v.getIsbn() + "\nTítulo: " + v.getTitulo());
                 });
             }
-
             if (option == 3){
                 String isbn = IO.readln("Entre com o ISBN: ");              
                 if (acervo.containsKey(isbn)){
                     System.out.println(acervo.get(isbn));
                 }
             }
-
             if (option == 4){
 
             }
@@ -50,8 +47,7 @@ public class App {
             if (option == 6){
                 String isbn = IO.readln("Entre com o ISBN: ");
                 if (acervo.containsKey(isbn)){
-                    Livro op3 = acervo.get(isbn);
-                    System.out.println(op3);
+                    acervo.remove(isbn);
                 }
             }
             
