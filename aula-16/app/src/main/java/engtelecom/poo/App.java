ebbc31074c618d93f2184ed56d4a69eb82a705bf
gustavo <gustavo.fa2005@aluno.ifsc.edu.br>
2026-04-06 08:58:39 -0300

    adicionado livro e sair

--- aula-16/app/src/main/java/engtelecom/poo/App.java
+++ aula-16/app/src/main/java/engtelecom/poo/App.java
@@ -4,14 +4,43 @@ import java.util.HashMap;
 
 public class App {
     HashMap<String, Livro> acervo = new HashMap<>();
-    public void livraria () {}
+    public void livraria () {
+        int option;
+        do {
+        /*
+        1- cadastrar livro
+    2- listar isbn & titulo
+    3- consultar livro por isbn, imprimir todos os dados
+    4- consultar livro por autor, imprimir todos os dados
+    5- atualizar dados do livro, menos isbn
+    6- remover livro por isbn
+        7- sair
+        */
+            option = Integer.parseInt(IO.readln("Escolha uma das opções:\n1-Criar livro\n7-Sair\n"));
+            if (option == 1){
+                String author = IO.readln("Entre com o Autor: ");
+                String isbn = IO.readln("Entre com o ISBN: ");
+                String titulo = IO.readln("Entre com o Titulo: ");
+                int ano = Integer.parseInt(IO.readln("Entre com o ano de publicação:"));
+                Livro a = new Livro(titulo, author, isbn, ano);
+                acervo.put(a.getIsbn(), a);
+            }
+
+            if (option == 2){
+                acervo.forEach((k, v)->{});
+            }
+
+            if (option == 7){System.out.println("Saindo...");}
+        
+        } while (option !=7);
+    }
     
     public static void main(String[] args) {
     App app = new App();
     app.livraria();
-    Livro a = new Livro("The Sculpture", "S.C.P.", "948", 2010);
-    Livro b = new Livro("The Hard-to-Kill Reptile", "S.C.P.", "244", 2010);
-    Livro c = new Livro("The Doctor", "S.C.P.", "073", 2010);
+    // Livro a = new Livro("The Sculpture", "S.C.P.", "948", 2010);
+    // Livro b = new Livro("The Hard-to-Kill Reptile", "S.C.P.", "244", 2010);
+    // Livro c = new Livro("The Doctor", "S.C.P.", "073", 2010);
     // // if (acervo.containsKey(a.getIsbn())){
     //     acervo.put(a.getIsbn(), a);
     // // }
@@ -25,15 +54,5 @@ public class App {
     // for (Livro liv: acervo.values()){
     //     IO.println(liv);
     // }
-
-    /*
-    1- cadastrar livro
-    2- listar isbn & titulo
-    3- consultar livro por isbn, imprimir todos os dados
-    4- consultar livro por autor, imprimir todos os dados
-    5- atualizar dados do livro, menos isbn
-    6- remover livro por isbn
-    7- sair
-    */
     }
 }
