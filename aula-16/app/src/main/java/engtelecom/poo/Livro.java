2f992d72e249de355318431ac8d3a29949dca9b5
gustavo <gustavo.fa2005@aluno.ifsc.edu.br>
2026-04-06 08:23:28 -0300

    livro.

---
+++ aula-16/app/src/main/java/engtelecom/poo/Livro.java
@@ -0,0 +1,36 @@
+package engtelecom.poo;
+
+public class Livro {
+    private String titulo;
+    private String autor;
+    private String isbn;
+    private int anoPublication;
+
+    public Livro(String titulo, String autor, String isbn, int anoPublication) {
+        setTitulo(titulo);
+        setAutor(autor);
+        setAnoPublication(anoPublication);
+        this.isbn = isbn;
+    }
+
+    public String getTitulo() {return titulo;}
+    public void setTitulo(String titulo) {this.titulo = titulo;}
+    public String getAutor() {return autor;}
+    public void setAutor(String autor) {this.autor = autor;}
+    public String getIsbn() {return isbn;}
+    public int getAnoPublication() {return anoPublication;}
+
+    public void setAnoPublication(int anoPublication) {
+        this.anoPublication = anoPublication;
+    }
+
+    public String toString(){
+        return String.format("""
+                ISBN: %s
+                Título: %s
+                Autor: %s
+                Ano de Publicação: %d
+                """, isbn, titulo, autor, anoPublication
+        );
+    }
+}
