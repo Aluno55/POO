af325b10dd82938f665f181850efccf2c81735e2
Gustavo Felipe de Andrade <gustavo.fa2005@aluno.ifsc.edu.br>
2026-04-08 20:01:15 -0300

    Acho que não entendi Stream

--- aula-08/app/src/main/java/engtelecom/poo/App.java
+++ aula-08/app/src/main/java/engtelecom/poo/App.java
@@ -1,41 +1,37 @@
 package engtelecom.poo;
-
+import java.util.Stack;
 import java.util.Arrays;
 import java.util.List;
 
 public class App {
     public static void main(String[] args) {
-        // List<Integer> lista = Arrays.asList(1, 2, 4, 5, 6);
-        // int soma = 0;
-        // soma = lista.stream()
-        //     .filter(numero -> numero % 2 == 0) // Filtra os números pares
-        //     .mapToInt(Integer::intValue) // Converte para IntStream
-        //     .sum(); // Soma os números
-        
-        //     // Código imperativo equivalente
-        // soma = 0;
-        // for (Integer numero : lista) {
-        //     if (numero % 2 == 0) {
-        //        soma += numero;
-        //     }
-        // }
 
-        List<String> lista = Arrays.asList("Java", "Stream", "API", "Collections");
-        // Usando lambda
-        int maior = lista.stream()
-        .mapToInt(p-> p.length()) // Converte cada palavra para seu comprimento (int)
-        .max() // Encontra o valor máximo entre os comprimentos
-        .orElse(0); // Retorna 0 se a lista estiver vazia
-
-        // Outra forma de escrever usando referência de método
-        int maior2 = lista.stream()
-        .mapToInt(String::length)
-        .max()
-        .orElse(0);
-        
-        int tam = 0;
-        for (int i = lista.size(); i > 0; i--) {
-            tam = (lista.get(i).length() > 0)? lista.get(i).length() : 0;
+        Stack<String> stacks = new Stack<>();
+        for (int i = 0; i < args.length; i++) {
+            stacks.push(IO.readln());
         }
+        var c = '(';
+        stacks.search(c);
+        
+        
+        
+
+        // List<String> lista = Arrays.asList("Java", "Stream", "API", "Collections");
+        // // Usando lambda
+        // int maior = lista.stream()
+        // .mapToInt(p-> p.length()) // Converte cada palavra para seu comprimento (int)
+        // .max() // Encontra o valor máximo entre os comprimentos
+        // .orElse(0); // Retorna 0 se a lista estiver vazia
+
+        // // Outra forma de escrever usando referência de método
+        // int maior2 = lista.stream()
+        // .mapToInt(String::length)
+        // .max()
+        // .orElse(0);
+        
+        // int tam = 0;
+        // for (int i = lista.size(); i > 0; i--) {
+        //     tam = (lista.get(i).length() > 0)? lista.get(i).length() : 0;
+        // }
     }
 }
