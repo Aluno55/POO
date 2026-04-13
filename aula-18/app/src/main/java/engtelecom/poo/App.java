d733ace73ab657966710c2cf69ad5ded3a321437
Gustavo <gustavo.fa2005@aluno.ifsc.edu.br>
2026-04-13 08:03:44 -0300

    mermaid

--- aula-18/app/src/main/java/engtelecom/poo/App.java
+++ aula-18/app/src/main/java/engtelecom/poo/App.java
@@ -1,8 +1,24 @@
 package engtelecom.poo;
 
 public class App {
-
     public static void main(String[] args) {
+// Diagrama +Getters e Setters omitidos
+//- estáticos são sublinhados
+//+Public -Private #???
+// 
+// Classe | -Atributos | +Métodos
+//
+// utilizar mermaid.ai/live ou draw.io
+// 
+// 
+// 
+// 
+// 
 
+
+
+
+
+        IO.println("Fim");
     }
 }
---
+++ aula-18/app/src/main/java/engtelecom/poo/README.md
@@ -0,0 +1,11 @@
+# Diagrama de Classe UML
+```mermaid
+classDiagram
+
+    class Retangulo{
+        - int altura
+        - int largura
+        + Retangulo(int a, int l)
+        + getArea() int
+    }
+```
