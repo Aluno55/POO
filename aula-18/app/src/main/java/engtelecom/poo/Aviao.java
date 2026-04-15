4d57927bb5a21291a49a39ad826ef0da8a76d270
Gustavo <gustavo.fa2005@aluno.ifsc.edu.br>
2026-04-15 10:58:43 -0300

    aviao

--- aula-18/app/src/main/java/engtelecom/poo/Aviao.java
+++ aula-18/app/src/main/java/engtelecom/poo/Aviao.java
@@ -1,19 +1,39 @@
 package engtelecom.poo;
 
+import java.util.ArrayList;
+
 public class Aviao {
+    private int tripulanteMax;
+    private int passageiroMax;
+    private double pesoMax;
+    private double combustivelMax;
+    private int numMotores;
+    private MotorAviao propulsor;
+    private String tipoMotor;
+    private ArrayList<MotorAviao> motores = new ArrayList<>();
+    
+    public boolean possivelDecolar(double peso, double combustible, int tripulante, int passageiro){
+        int variaveis = 0;
+        variaveis = (peso < pesoMax)? +1 : 0;
+        variaveis = (combustible < combustivelMax)? +1 : 0;
+        variaveis = (tripulante < tripulanteMax)? +1 : 0;
+        variaveis = (passageiro < passageiroMax)? +1 : 0;
+        return (variaveis == 4);
+    }
+
+    public Aviao(double combustivelMax, int numMotores, MotorAviao propulsor, String tipoMotor) {
+        this.numMotores = numMotores;
+        this.propulsor = propulsor;
+        this.tipoMotor = tipoMotor;
+        
+        motores.add(propulsor);
+        
+        this.combustivelMax = combustivelMax;
+    }
+
+    public void acelerar(int v){
+
+
+    }
     
 }
-
-// class Aviao{
-//         - int tripulante
-//         - int passageiro
-//         - MotorAviao propulsor
-//         - int numMotores
-//         - ArrayList~Motor~ motores
-//         - String tipoMotor
-//         - double pesoMax
-//         - double combustivelMax
-//         + acelerar(v: int) void
-//         + possivelDecolar() boolean
-//         + Aviao(combustivelMax: double, pesoMax: double, numMotores: int, tipoMotor:String)
-//     }
