22242d05e0b33eeb7a6f301a9518436ed8f22f7e
Gustavo Felipe de Andrade <gustavo.fa2005@aluno.ifsc.edu.br>
2026-04-23 14:17:56 -0300

    Agena 1

--- aula-20/app/src/main/java/engtelecom/poo/Agenda.java
+++ aula-20/app/src/main/java/engtelecom/poo/Agenda.java
@@ -4,4 +4,13 @@ import java.util.ArrayList;
 
 public class Agenda {
     private ArrayList<Contato> accounts;
+
+    public boolean addContato (Contato c){
+        accounts.add(c);
+        return true;
+    }
+    public boolean removeContato (int indiceNaLista){
+        accounts.get(indiceNaLista);
+        return true;
+    }
 }
--- aula-20/app/src/main/java/engtelecom/poo/App.java
+++ aula-20/app/src/main/java/engtelecom/poo/App.java
@@ -1,14 +1,20 @@
 package engtelecom.poo;
+import java.time.LocalDate;
 
 public class App {
     public void menu(){
         int option = 1;
-        Contato c;
+        Agenda agenda = new Agenda();
         do {
-            option = Integer.parseInt(IO.readln("Escolha uma das opções:\n0-sair\n"));
-            if (option<0) {
-                
+            option = Integer.parseInt(IO.readln("Escolha uma das opções:\n0-Sair\n1-Adicionar Contato\n"));
+            if (option == 1){
+                var name = IO.readln("Qual sobrenome?");
+                var surname = IO.readln("Qual nome?");
+                LocalDate date = LocalDate.parse(IO.readln("Qual nome?"));
+                Contato conta = new Contato(surname, name, date);
+                agenda.addContato(conta);
             }
+            else {IO.println("Opção invalida");}
         } while (option != 0);
     }
     public static void main(String[] args) {
