e13ccafc5072c6325dcace6562cd73343867d7a4
Gustavo Felipe de Andrade <gustavo.fa2005@aluno.ifsc.edu.br>
2026-04-23 17:15:07 -0300

    App

--- aula-20/app/src/main/java/engtelecom/poo/App.java
+++ aula-20/app/src/main/java/engtelecom/poo/App.java
@@ -6,7 +6,7 @@ public class App {
         int option = 1;
         Agenda agenda = new Agenda();
         do {
-            option = Integer.parseInt(IO.readln("Escolha uma das opções:\n0-Sair\n1-Adicionar Contato\n"));
+            option = Integer.parseInt(IO.readln("Escolha uma das opções:\n0-Sair\n1-Adicionar Contato\n2-Remover Contato\n3-Configurar Emails\n4-Configurar Telefones"));
             switch (option) {
                 case 1:
                     var name = IO.readln("Qual sobrenome?");
@@ -16,7 +16,27 @@ public class App {
                     agenda.addContato(conta);
                 break;
                 case 2:
+                    int num = Integer.parseInt(IO.readln("Qual a posição do contato?"));
+                    agenda.removeContato(num);
                 break;
+                case 3:
+                    int email = Integer.parseInt(IO.readln("1-Adicionar Email\n2-Remover Email\n3-Atualizar Email"));
+                    // switch (email) {
+                    //     case 1:
+                            
+                    //     break;
+                    //     case 2:
+
+                    //     break;
+                    //     default:
+                    //         IO.println("Opção invalida");
+                    //     break;
+                    // }
+                    break;
+                case 4:
+                    int phone = Integer.parseInt(IO.readln("1-Adicionar Telefone\n2-Remover Telefone\n3-Atualizar Telefone"));
+                
+                    break;
                 default:
                     IO.println("Opção invalida");
                 break;
@@ -24,8 +44,9 @@ public class App {
         } while (option != 0);
     }
     public static void main(String[] args) {
+        App menu = new App();
+        menu.menu();
         //tem entidades, atributos e relacionamentos
         //associação, exemplo: alunos usam matriculas que usa cursos
-    
     }
 }
