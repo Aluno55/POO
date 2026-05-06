e10e1521e431fbe629ea53ed5da2b8df75161316
Gustavo <gustavo.fa2005@aluno.ifsc.edu.br>
2026-05-06 10:29:20 -0300

    circulo

--- lab-05/ex1/app/src/main/java/engtelecom/poo/App.java
+++ lab-05/ex1/app/src/main/java/engtelecom/poo/App.java
@@ -1,9 +1,9 @@
 package engtelecom.poo;
-// import edu.princeton.cs.algs4.Draw;
+import edu.princeton.cs.algs4.Draw;
 
 public class App {
     public static void main(String[] args) {
-        // Draw art = new Draw();
-        // art.circle(0, 0, 3);
+        Draw art = new Draw();
+        art.circle(0.5, 0.5, 0.3);
     }
 }
