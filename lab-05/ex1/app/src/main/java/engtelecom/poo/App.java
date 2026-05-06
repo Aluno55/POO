dd1a0550307f47e7e754c53fe9d635df211f80d9
Gustavo <gustavo.fa2005@aluno.ifsc.edu.br>
2026-05-06 10:38:27 -0300

    formas e cores

--- lab-05/ex1/app/src/main/java/engtelecom/poo/App.java
+++ lab-05/ex1/app/src/main/java/engtelecom/poo/App.java
@@ -1,9 +1,17 @@
 package engtelecom.poo;
+import java.awt.Color;
 import edu.princeton.cs.algs4.Draw;
 
 public class App {
     public static void main(String[] args) {
         Draw art = new Draw();
-        art.circle(0.5, 0.5, 0.3);
+        art.setXscale(0, 800);
+        art.setYscale(0, 800);
+        art.setCanvasSize(800,800);
+        art.setPenColor(Color.blue);
+        art.circle(400, 400, 100);
+        art.setPenColor(Color.red);
+        art.rectangle(100, 700, 70, 40);
+        art.filledCircle(50, 50, 50);
     }
 }
