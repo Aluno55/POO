15ab9539684d8ef1b1bc0b49e9ecae9ae6cdaeb5
Gustavo <gustavo.fa2005@aluno.ifsc.edu.br>
2026-05-06 10:25:18 -0300

    ensinando um artista a desenhar

--- lab-05/ex1/app/src/main/java/engtelecom/poo/App.java
+++ lab-05/ex1/app/src/main/java/engtelecom/poo/App.java
@@ -1,7 +1,9 @@
 package engtelecom.poo;
+import edu.princeton.cs.algs4.Draw;
 
 public class App {
     public static void main(String[] args) {
-
+        Draw art = new Draw();
+        art.circle(0, 0, 3);
     }
 }
