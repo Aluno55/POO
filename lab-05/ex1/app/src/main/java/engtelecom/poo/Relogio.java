ddd17b42584954fe4cae5fbd321cbd8f818ac4c5
Gustavo Felipe de Andrade <gustavo.fa2005@aluno.ifsc.edu.br>
2026-05-07 21:16:48 -0300

    relogio disfuncional

--- lab-05/ex1/app/src/main/java/engtelecom/poo/Relogio.java
+++ lab-05/ex1/app/src/main/java/engtelecom/poo/Relogio.java
@@ -16,6 +16,7 @@ public class Relogio {
         displays = new ArrayList<>();
     }
 
+    // val é para definir o tamanho do relogio, ver onde coloco depois
     public void counter(int val) throws InterruptedException{
         Draw art = new Draw();
         art.setXscale(0, 1000);
@@ -29,16 +30,48 @@ public class Relogio {
             art.line(i, 0, i, 1000);
             art.line(0, i, 1000, i);
         }
+        int repeatSec = 0;
+        int repeatMin = 0;
+        int repeatHrs = 0;
+
         for (int i = 0; i < 3600; i++) {
             art.clear();
             for (int j = 0; j < 60; j++) {
                 for (int j2 = 0; j2 < 60; j2++) {
+                    if (segundos>=9){
+                        segundos = 0;
+                        repeatSec++;
+                        displays.get(1).setSegmento(repeatSec);
+                    }
+                    displays.get(0).setSegmento(segundos);
                     segundos++;
                     TimeUnit.SECONDS.sleep(1);
                 }
-                minutos++;
+                if (repeatSec >= 6) {
+                    repeatSec = 0;
+                    displays.get(2).setSegmento(minutos);
+                    minutos++;
+                }
+                if (minutos >= 9) {
+                    repeatMin++;
+                    minutos = 0;
+                    displays.get(3).setSegmento(repeatMin);
+                }
             }
-            horas++;
+            if (repeatMin >= 6){
+                repeatMin = 0;
+                horas++;
+                displays.get(4).setSegmento(horas);
+            }
+            if (horas == 4 && repeatHrs == 2){
+                horas = repeatHrs = 0;
+                minutos = repeatMin = 0;
+                segundos = repeatSec = 0;
+            } else if (horas >= 9){
+                repeatHrs++;
+                horas = 0;
+                displays.get(5).setSegmento(horas);
+            }
             art.show();
         }
     }
