f70cefca76ed4cb5d5f2c73232dc27e5025165a6
Gustavo Felipe de Andrade <gustavo.fa2005@aluno.ifsc.edu.br>
2026-05-20 14:26:29 -0300

    digital texto

---
+++ lab-06/app/src/main/java/engtelecom/poo/DigitalSeg.java
@@ -0,0 +1,9 @@
+package engtelecom.poo;
+
+public class DigitalSeg extends Cronometro{
+
+    public DigitalSeg(int hora, int minuto, int segundo) {
+        super(hora, minuto, segundo);
+    }
+    
+}
