3b6daac2575089f2ebf045b0fdac56869fd2bdc4
Gustavo <gustavo.fa2005@aluno.ifsc.edu.br>
2026-06-03 11:11:07 -0300

    mini lab

--- aula-24/app/src/main/java/engtelecom/poo/App.java
+++ aula-24/app/src/main/java/engtelecom/poo/App.java
@@ -1,36 +1,49 @@
 package engtelecom.poo;
+import java.text.ParseException;
 import java.util.InputMismatchException;
 import java.util.Scanner;
+import javax.swing.text.MaskFormatter;
 
 public class App {
+    static String formatar (String mascara, String valor){
+        try {
+            MaskFormatter mask = new MaskFormatter(mascara);
+        } catch (ParseException e) {
+            System.out.println(e);
+        } finally {}
+        // try () {} catch (Exception e) {}
+        return "";
+    }
+
     public static void main(String[] args) {
-        // Caixa<String> c = new Caixa<String>("Olá mundo");
-        // Caixa<Pessoa> p = new Caixa<Pessoa>(new Pessoa("Joao"));
-        // String s = c.get();
-        // String errado = p.get().getNome();
+        String s = formatar("(##)", "42");
+        IO.println(s);
+
+        /*Caixa<String> c = new Caixa<String>("Olá mundo");
+        Caixa<Pessoa> p = new Caixa<Pessoa>(new Pessoa("Joao"));
+        String s = c.get();
+        String errado = p.get().getNome();*/
         
-        Scanner ler = new Scanner(System.in);
+        /*Scanner ler = new Scanner(System.in);
         int a, b;
         try{
             System.out.println("Entre com o numero: ");
             a = ler.nextInt();
-            System.out.println("Entre com o numero: ");
+        System.out.println("Entre com o numero: ");
             b = ler.nextInt();
             int res = a/b;
             System.out.println(a + " dividido por " + b + " é " + res);
-        }
-        catch(InputMismatchException e){
+        }catch(InputMismatchException e){
             System.err.println("Só é permitido numeros inteiros");
             ler.nextLine();
-        }
-        catch(ArithmeticException e){
+        }catch(ArithmeticException e){
             System.err.println("Divisão indefinida");
             ler.nextLine();
-        }
-        catch(Exception e){
+        }catch(Exception e){
             System.err.println("Tu foi tão péssimo que conseguiu errar isso como?");
             ler.nextLine();
-        }
+        // }
+        */
         System.out.println("Fim do Programa");
     }
 }
