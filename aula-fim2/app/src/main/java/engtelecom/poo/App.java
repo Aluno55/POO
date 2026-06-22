48d0cb611bd8980a0d09985dbf7dd4230cdd4fc3
gustavo <gustavo.fa2005@aluno.ifsc.edu.br>
2026-06-22 08:29:20 -0300

    arquivos

--- aula-fim2/app/src/main/java/engtelecom/poo/App.java
+++ aula-fim2/app/src/main/java/engtelecom/poo/App.java
@@ -1,7 +1,31 @@
 package engtelecom.poo;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
 
+        try(Scanner arq = new Scanner(App.class.getClassLoader().getResourceAsStream("texto.txt"))){
+            while (arq.hasNext()){
+                IO.println(arq.nextLine());
+            }
+        } catch (Exception e){
+            System.err.println("Erro: " + e.toString());
+        }
+
+/*
+arquivo regulares: arquivos contendo imagens, texto, etc.
+arquivo diretório: arquivos contendo outros arquivos. Uma "pasta"
+
+caminho absoluto: ex. home/aula/POO/aula-fim2/app/src/main/java/engtelecom/poo/App.java
+caminho relativo: ex. ../poo/App.java ou app\App.java
+
+arquivo binário: eficiente armazenamento, imagem, áudio, vídeo, etc.
+arquivo texto: usa caracteres, texto, csv, markdown, etc.
+
+the "resources" directory is used to save resources, like audios, images and the such.
+
+Path path = Path.of("src/main/resources/coisas"); // InputStream is = App.class.getClassLoader().getResourceAsStream();
+*/
     }
 }
