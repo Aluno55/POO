910beac1e0cf2e4f8877c0964c841283ef830f79
Gustavo Felipe de Andrade <gustavo.fa2005@aluno.ifsc.edu.br>
2026-06-22 20:16:38 -0300

    mixing the last 2 "aula"s

--- aula-fim/app/src/main/java/engtelecom/poo/App.java
+++ aula-fim/app/src/main/java/engtelecom/poo/App.java
@@ -1,7 +1,49 @@
 package engtelecom.poo;
+import java.nio.file.Path;
 
 public class App {
-    public static void main(String[] args) {
+    public void aplicativo(){
+        String currentDir = System.getProperty("user.dir");
+        Path dir = Path.of(currentDir);
+        Programa prog = new Programa();
+        String option = "";
         
+        do {
+            option = IO.readln("ls: lista o conteúdo no diretório atual\n"+
+            "stat <Arquivo>: imprime propriedades do arquivo\n"+
+            "touch <Arquivo>: cria um arquivo texto\n"+
+            "rm <Arquivo>: exclui o arquivo indicado\n"+
+            "mv <Arquivo1> <Arquivo 2>: move o arquivo1 para o arquivo2"+
+            "\nexit: sair do programa");
+            option = option.toLowerCase();
+
+            switch (option) {
+                case "ls":
+                    prog.ls(dir);
+                break;
+                case "stat":
+                    prog.stat(dir);
+                break;
+                case "touch":
+                    prog.touch(dir);
+                break;
+                case "rm":
+                    prog.rm(dir);
+                break;
+                case "mv":
+                    var newDir = IO.readln("Insira o segundo arquivo");
+                    Path dir2 = Path.of(newDir);
+                    prog.mv(dir, dir2);
+                break;
+                default:
+                    IO.println("Invalido");
+                break;
+            }
+        } while (option.equalsIgnoreCase("exit"));
+    }
+
+    public static void main(String[] args) {
+        App app = new App();
+        app.aplicativo();
     }
 }
