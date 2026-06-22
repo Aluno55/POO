2d8b107883d7410b37e5c681118d776d115ec2f8
Gustavo Felipe de Andrade <gustavo.fa2005@aluno.ifsc.edu.br>
2026-06-22 15:01:13 -0300

    aplicativo relacionado a arquivos

--- aula-fim2/app/src/main/java/engtelecom/poo/App.java
+++ aula-fim2/app/src/main/java/engtelecom/poo/App.java
@@ -3,39 +3,43 @@ import java.nio.file.Path;
 
 public class App {
     public void aplicativo(){
-        String option = IO.readln("ls: lista o conteúdo no diretório atual\n"+
-        "stat <Arquivo>: imprime propriedades do arquivo\n"+
-        "touch <Arquivo>: cria um arquivo texto\n"+
-        "rm <Arquivo>: exclui o arquivo indicado\n"+
-        "mv <Arquivo1> <Arquivo 2>: move o arquivo1 para o arquivo2");
         String currentDir = System.getProperty("user.dir");
         Path dir = Path.of(currentDir);
         Programa prog = new Programa();
+        String option = "";
         
-        option = option.toLowerCase();
+        do {
+            option = IO.readln("ls: lista o conteúdo no diretório atual\n"+
+            "stat <Arquivo>: imprime propriedades do arquivo\n"+
+            "touch <Arquivo>: cria um arquivo texto\n"+
+            "rm <Arquivo>: exclui o arquivo indicado\n"+
+            "mv <Arquivo1> <Arquivo 2>: move o arquivo1 para o arquivo2"+
+            "\nexit: sair do programa");
+            option = option.toLowerCase();
 
-        switch (option) {
-            case "ls":
-                prog.ls(dir);
+            switch (option) {
+                case "ls":
+                    prog.ls(dir);
                 break;
-            case "stat":
-                prog.stat(dir);
+                case "stat":
+                    prog.stat(dir);
                 break;
-            case "touch":
-                prog.touch(dir);
+                case "touch":
+                    prog.touch(dir);
                 break;
-            case "rm":
-                prog.rm(dir);
+                case "rm":
+                    prog.rm(dir);
                 break;
-            case "mv":
-                var newDir = IO.readln("Insira o segundo arquivo");
-                Path dir2 = Path.of(newDir);
-                prog.mv(dir, dir2);
+                case "mv":
+                    var newDir = IO.readln("Insira o segundo arquivo");
+                    Path dir2 = Path.of(newDir);
+                    prog.mv(dir, dir2);
                 break;
-            default:
-                IO.println("Invalido");
+                default:
+                    IO.println("Invalido");
                 break;
-        }
+            }
+        } while (option.equalsIgnoreCase("exit"));
     }
 
     public static void main(String[] args) {
